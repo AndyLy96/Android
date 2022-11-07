4d32e5dfef09c05c2d93dd8fe522691265511304
Andy <2157338@cegepmontpetit.ca>
2022-11-07 15:26:00 -0500

    progrès

---
+++ StockageDernierArret/app/src/main/java/org/sabourin/bd/MonDao.java
@@ -0,0 +1,24 @@
+package org.sabourin.bd;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import org.sabourin.modele.VDQuestion;
+import org.sabourin.modele.VDVote;
+
+import java.util.List;
+
+@Dao
+public interface MonDao {
+    @Insert
+    Long insertQuestion(VDQuestion v);
+
+    @Insert
+    Long insertTime(VDQuestion v);
+
+
+
+    //TODO Compléter les autres actions
+
+}
